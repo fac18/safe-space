2cc6bfdc677aaf1d3dcb518e605fe0565c138593
groupanimal <groupanimal@posteo.net>
2020-02-11 15:59:52 +0000

    improve commenting in getData

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,8 +1,7 @@
 const getQuestions = async () => {
   // get-questions.js is async so returns a promise
-  // therefore fetch returns a promise which resolves to a promise
   // first we wait for the fetch to resolve to a response
-  // then we wait for the response to stream as json
+  // then we wait for the response to stream as json, and return the result
   return await (
     await fetch('.netlify/functions/get-questions/get-questions.js')
   ).json();
