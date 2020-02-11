e8e862fb70b30d535f44778b1f69b5eb8c25e12c
Alexandra O'Mahony <alexandra.omahony1@gmail.com>
2020-02-11 11:19:14 +0000

    Added component folders, js files and test files

---
+++ src/components/Footer/footer.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Footer from './Footer';
+
+test('The Footer renders', () => {
+  render(<Footer></Footer>);
+});
---
+++ src/components/Header/Header.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const Header = () => {
+  return <p>Header goes here</p>;
+};
+
+export default Header;
