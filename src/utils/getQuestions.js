4a1c8befb03b4ae0edcc4948227992ca572dabe5
groupanimal <groupanimal@posteo.net>
2020-02-11 15:56:13 +0000

    remove hello-world functional and corresponding util move useful comments across to get-questions function etc.

--- src/utils/getQuestions.js
+++ src/utils/getQuestions.js
@@ -1,4 +1,8 @@
 const getQuestions = async () => {
+  // get-questions.js is async so returns a promise
+  // therefore fetch returns a promise which resolves to a promise
+  // first we wait for the fetch to resolve to a response
+  // then we wait for the response to stream as json
   return await (
     await fetch('.netlify/functions/get-questions/get-questions.js')
   ).json();
