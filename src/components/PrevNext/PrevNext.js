d2f91fdb1dd0af4e1ac86f2fddccf87ee0b4c2c1
Reda <redahaq@gmail.com>
2020-02-17 12:28:30 +0000

    create next and back button components with styling

--- src/components/PrevNext/PrevNext.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-const PrevNext = () => {
-  return <p>PrevNext goes here</p>;
-};
-
-export default PrevNext;
--- src/components/PrevNext/PrevNext.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import PrevNext from './PrevNext';
-
-test('The PrevNext renders', () => {
-  render(<PrevNext></PrevNext>);
-});
