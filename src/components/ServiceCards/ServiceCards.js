12f17df2d4411cdd25586aeb341e59c2488dba7a
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-02-19 12:27:40 +0000

    Set up card component in Support.js and compile data on services for props

---
+++ src/components/ServiceCards/ServiceCards.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import { Card } from '@rmwc/card';
+
+const ServiceCard = styled(Card)`
+  && {
+    border: 2px solid #CA68A4;
+    border-radius: 3rem;
+    background: #d5d8e8;
+    font-weight: bold;
+    width: 15rem;
+    height: 10rem;
+    margin: auto;
+    margin-top: 2rem;
+  }
+`;
+
+export default ServiceCard;
--- src/pages/Support.js
+++ src/pages/Support.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+import ServiceCard from '../components/ServiceCards/ServiceCards.js';
+import { Typography } from '@rmwc/typography';
+
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  margin-top: 1rem;
+
+`;
+
 
 const Support = () => {
-  return <h1>This is Support Page</h1>;
-};
+  return (
+  <Container>
+    <Typography use='headline3'> Support Services </Typography>
+    <ServiceCard />
+  </Container>
+)};
 export default Support;
