0183a8717e2b85350f47ad2e695da3a79754b5fd
Reda <redahaq@gmail.com>
2020-02-20 11:50:31 +0000

    try fixing bubble overflow

--- src/components/style.js
+++ src/components/style.js
@@ -1,13 +1,26 @@
+import React from 'react';
 import { ReactComponent as HeaderBubbles } from '../assets/Header-bubbles.svg';
 import { ReactComponent as LowerBubbles } from '../assets/Footer-bubbles.svg';
 import styled from 'styled-components';
 
-const Bubbles = styled(HeaderBubbles)`
+// const Bubbles = styled(HeaderBubbles)`
+
+// `;
+
+const BubblesContainer = styled.div`
+  overflow: hidden;
   position: absolute;
   left: -4em;
   top: -104px;
-  overflow: hidden;
 `;
+
+const Bubbles = () => {
+  return (
+    <BubblesContainer>
+      <HeaderBubbles />
+    </BubblesContainer>
+  );
+};
 
 const FooterBubbles = styled(LowerBubbles)`
   position: absolute;
