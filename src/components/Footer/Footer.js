60244fd118fde3f16bb8bcc7db22f98abc6455fa
Reda <39345463+redahaq@users.noreply.github.com>
2020-02-21 11:34:54 +0000

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import { TypeQ } from '../style';
 
 const Bottom = styled.footer`
   display: flex;
-  /* position: absolute; FIX THIS*/ 
+  position: absolute;
   flex-direction: column;
   align-items: flex-end;
   padding: 0.5rem;
