33ef2c69b02df026f0fca9446e341cf6a996a87b
Reda <redahaq@gmail.com>
2020-03-29 14:48:14 +0100

    feat(create collapsible FAQs)

--- src/App/FAQs/FAQs.js
+++ src/App/FAQs/FAQs.js
@@ -22,6 +22,38 @@ const Type5 = styled(Typography)`
   }
 `;
 
+function Collapse(props) {
+  const [isCollapsed, setIsCollapsed] = React.useState(props.collapsed);
+  const style = {
+    collapsed: {
+      display: 'none',
+    },
+    expanded: {
+      display: 'block',
+    },
+    buttonStyle: {
+      display: 'block',
+      width: '100%',
+    },
+  };
+  return (
+    <div>
+      <button
+        style={style.buttonStyle}
+        onClick={() => setIsCollapsed(!isCollapsed)}
+      >
+        {isCollapsed ? 'Show' : 'Hide'} content
+      </button>
+      <div
+        className='collapse-content'
+        style={isCollapsed ? style.collapsed : style.expanded}
+        aria-expanded={isCollapsed}
+      >
+        {props.children}
+      </div>
+    </div>
+  );
+}
 const FAQs = () => {
   return (
     <>
@@ -30,110 +62,122 @@ const FAQs = () => {
           <Header />
           <FormContainer>
             <FlexColumn>
-              <TypeQ use='headline5'>What is sexual harassment?</TypeQ>
-
-              <Type5 use='body1'>
-                Sexual harassment is when someone behaves in a way that makes
-                you feel distressed, intimidated or offended and the behaviour
-                is of a sexual nature.
-              </Type5>
-              <TypeQ use='headline6'>This includes:</TypeQ>
-              <Type5 use='body1'>Sexual comments or jokes</Type5>
-              <Type5 use='body1'>
-                Physical behaviour: unwelcome sexual advances, touching and
-                various forms of sexual assault.
-              </Type5>
-              <Type5 use='body1'>
-                Displaying photos, pictures or drawings of a sexual nature.
-              </Type5>
-              <Type5 use='body1'>
-                Sending messages, emails etc. with sexual content.
-              </Type5>
-              <Type5 use='body1'>
-                Learn more on <a href='http://themu.org/'>theMU.org</a>
-              </Type5>
-              <TypeQ use='headline5'>What is sexual assault?</TypeQ>
-              <Type5 use='body1'>
-                Sexual assault is any type of sexual activity or contact that is
-                not consented to
-              </Type5>
-              <TypeQ use='headline5'>Is the MU Safe Space for you?</TypeQ>
-              <Type5 use='body1'>
-                We want to hear from you if you already make or wish to make all
-                or part of your living from music. You might be a student, a
-                professional; be employed, self employed, not working at the
-                moment, or looking for work. We also want to hear from you if
-                you are an amateur musician. You might be playing in a community
-                orchestra or performing as part of a choir.
-              </Type5>
-              <Type5 use='body1'>
-                We need as many musicians as possible to share their
-                experiences. The more experiences musicians share, the easier it
-                will be for us to spot patterns and identify where we need to
-                focus our work to create meaningful and lasting change.
-              </Type5>
-              <Type5 use='body1'>
-                If you are aged 16 or under, or the incident happened when you
-                were 16 or under, it will not be covered by this survey. Please
-                contact the <a href='https://rapecrisis.org.uk/'>Rape Crisis</a>{' '}
-                or the <a href='https://www.nspcc.org.uk/'>NSPCC</a> for advice
-                and support. You can also find a list of organisations at{' '}
-                <a href='http://themu.org/safespace'>theMU.org/safespace</a>
-              </Type5>
-              <Type5 use='body1'>
-                If you need an immediate help and support, check the services
-                you can contact now:{' '}
-                <a href='https://www.musiciansunion.org.uk/Home/Advice/Your-Career/Sexual-Harassment-and-Discrimination/help-and-support'>
-                  MU Help & Support
-                </a>
-              </Type5>
-              <TypeQ use='headline5'>What the MU Safe Space can do</TypeQ>
-              <TypeQ use='headline6'>For MU members:</TypeQ>
-              <Type5 use='body1'>We can advise you on your legal rights</Type5>
-              <Type5 use='body1'>
-                We can provide information about relevant support services.
-              </Type5>
-              <Type5 use='body1'>
-                We can support you if you would like to seek redress.
-              </Type5>
-              <Type5 use='body1'>
-                We may be able to raise a complaint with the employer, engager,
-                or even directly with the perpetrator.
-              </Type5>
-              <Type5 use='body1'>
-                If both the alleged perpetrator and the complainant are MU
-                members, then we also have the option of disciplinary action
-                under MU rules.
-              </Type5>
-              <Type5 use='body1'>
-                Please keep record of the unique ID number given at the end of
-                the questions, this can be used later to reference your report
-                if you so wish.
-              </Type5>
-              <TypeQ use='headline6'>For everyone:</TypeQ>
-              <Type5 use='body1'>
-                We can provide advice on what your rights are.
-              </Type5>
-              <Type5 use='body1'>
-                We can provide information about relevant support services.
-              </Type5>
-              <Type5 use='body1'>
-                In cases of bullying, discrimination and other inappropriate
-                behaviour we can provide advice on your options and further
-                steps.
-              </Type5>
-              <TypeQ use='headline5'>What the MU Safe Space cannot do</TypeQ>
-              <Type5 use='body1'>
-                We cannot provide counselling. But we will always provide
-                information on relevant support services.
-              </Type5>
-              <Type5 use='body1'>
-                We are very limited in terms of what can be done industrially to
-                address a complaint of a sexual assault.
-              </Type5>
-              <Type5 use='body1'>
-                We cannot guarantee the outcome that you want.
-              </Type5>
+              <Collapse>
+                <TypeQ use='headline5'>What is sexual harassment?</TypeQ>
+                <Type5 use='body1'>
+                  Sexual harassment is when someone behaves in a way that makes
+                  you feel distressed, intimidated or offended and the behaviour
+                  is of a sexual nature.
+                </Type5>
+                <TypeQ use='headline6'>This includes:</TypeQ>
+                <Type5 use='body1'>Sexual comments or jokes</Type5>
+                <Type5 use='body1'>
+                  Physical behaviour: unwelcome sexual advances, touching and
+                  various forms of sexual assault.
+                </Type5>
+                <Type5 use='body1'>
+                  Displaying photos, pictures or drawings of a sexual nature.
+                </Type5>
+                <Type5 use='body1'>
+                  Sending messages, emails etc. with sexual content.
+                </Type5>
+                <Type5 use='body1'>
+                  Learn more on <a href='http://themu.org/'>theMU.org</a>
+                </Type5>
+              </Collapse>
+              <Collapse>
+                <TypeQ use='headline5'>What is sexual assault?</TypeQ>
+                <Type5 use='body1'>
+                  Sexual assault is any type of sexual activity or contact that
+                  is not consented to
+                </Type5>
+              </Collapse>
+              <Collapse>
+                <TypeQ use='headline5'>Is the MU Safe Space for you?</TypeQ>
+                <Type5 use='body1'>
+                  We want to hear from you if you already make or wish to make
+                  all or part of your living from music. You might be a student,
+                  a professional; be employed, self employed, not working at the
+                  moment, or looking for work. We also want to hear from you if
+                  you are an amateur musician. You might be playing in a
+                  community orchestra or performing as part of a choir.
+                </Type5>
+                <Type5 use='body1'>
+                  We need as many musicians as possible to share their
+                  experiences. The more experiences musicians share, the easier
+                  it will be for us to spot patterns and identify where we need
+                  to focus our work to create meaningful and lasting change.
+                </Type5>
+                <Type5 use='body1'>
+                  If you are aged 16 or under, or the incident happened when you
+                  were 16 or under, it will not be covered by this survey.
+                  Please contact the{' '}
+                  <a href='https://rapecrisis.org.uk/'>Rape Crisis</a> or the{' '}
+                  <a href='https://www.nspcc.org.uk/'>NSPCC</a> for advice and
+                  support. You can also find a list of organisations at{' '}
+                  <a href='http://themu.org/safespace'>theMU.org/safespace</a>
+                </Type5>
+                <Type5 use='body1'>
+                  If you need an immediate help and support, check the services
+                  you can contact now:{' '}
+                  <a href='https://www.musiciansunion.org.uk/Home/Advice/Your-Career/Sexual-Harassment-and-Discrimination/help-and-support'>
+                    MU Help & Support
+                  </a>
+                </Type5>
+              </Collapse>
+              <Collapse>
+                <TypeQ use='headline5'>What the MU Safe Space can do</TypeQ>
+                <TypeQ use='headline6'>For MU members:</TypeQ>
+                <Type5 use='body1'>
+                  We can advise you on your legal rights
+                </Type5>
+                <Type5 use='body1'>
+                  We can provide information about relevant support services.
+                </Type5>
+                <Type5 use='body1'>
+                  We can support you if you would like to seek redress.
+                </Type5>
+                <Type5 use='body1'>
+                  We may be able to raise a complaint with the employer,
+                  engager, or even directly with the perpetrator.
+                </Type5>
+                <Type5 use='body1'>
+                  If both the alleged perpetrator and the complainant are MU
+                  members, then we also have the option of disciplinary action
+                  under MU rules.
+                </Type5>
+                <Type5 use='body1'>
+                  Please keep record of the unique ID number given at the end of
+                  the questions, this can be used later to reference your report
+                  if you so wish.
+                </Type5>
+                <TypeQ use='headline6'>For everyone:</TypeQ>
+                <Type5 use='body1'>
+                  We can provide advice on what your rights are.
+                </Type5>
+                <Type5 use='body1'>
+                  We can provide information about relevant support services.
+                </Type5>
+                <Type5 use='body1'>
+                  In cases of bullying, discrimination and other inappropriate
+                  behaviour we can provide advice on your options and further
+                  steps.
+                </Type5>
+              </Collapse>
+              <Collapse>
+                <TypeQ use='headline5'>What the MU Safe Space cannot do</TypeQ>
+                <Type5 use='body1'>
+                  We cannot provide counselling. But we will always provide
+                  information on relevant support services.
+                </Type5>
+                <Type5 use='body1'>
+                  We are very limited in terms of what can be done industrially
+                  to address a complaint of a sexual assault.
+                </Type5>
+                <Type5 use='body1'>
+                  We cannot guarantee the outcome that you want.
+                </Type5>
+              </Collapse>
               <ButtonBack tag={Link} to={'/'}>
                 Home
               </ButtonBack>
@@ -145,4 +189,5 @@ const FAQs = () => {
     </>
   );
 };
+
 export default FAQs;
