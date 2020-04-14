e56245397e72bdac86164aeaa36e067b16c9a7d9
edificex <edificex@posteo.net>
2020-04-14 18:40:36 +0100

    remove console.logs throughout codebase replace hard-coded backups for questions, dividers, FAQ questions and support services

--- functions/get-data/get-data.js
+++ functions/get-data/get-data.js
@@ -8,7 +8,6 @@ exports.handler = async (event, context) => {
   const { AIRTABLE_API_KEY, AIRTABLE_BASE_ID, AIRTABLE_API_URL } = process.env;
   // grab table from event object (submitted via query string in fetch made from util)
   const table = event.queryStringParameters.table;
-  console.log(`getting data from table ${table}`);
 
   // configure connection to airtable base
   const base = new Airtable({
@@ -26,13 +25,13 @@ exports.handler = async (event, context) => {
       view: 'Grid view',
     })
     .firstPage()
-    .then(records => {
-      records.forEach(record => {
+    .then((records) => {
+      records.forEach((record) => {
         data.push(record.fields);
       });
     })
-    .catch(err => {
-      console.log(err.status);
+    .catch((err) => {
+      console.log(err.status); // only visible in netlify functions log when running in prod
     });
 
   // finally return any data collected as body of a response object
