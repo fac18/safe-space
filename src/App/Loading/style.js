6ff4daddca3d1c99524c09ba2e123d6e8354fdb8
edificex <edificex@posteo.net>
2020-04-15 18:01:19 +0100

    fix Loading text deal with React warnings about unnecessary imports

---
+++ src/App/Loading/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const CenterWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  margin: auto;
+  align-items: center;
+  justify-content: center;
+  align-content: center;
+`;
+
+export { CenterWrapper };
