edb69e5c30ad81814ac812e40b4b13de0d8e084a
MusiciansUnion <50022145+MusiciansUnion@users.noreply.github.com>
2024-02-22 16:23:56 +0000

    Update Footer.js

--- src/App/Footer/Footer.js
+++ src/App/Footer/Footer.js
@@ -5,15 +5,16 @@ const Footer = () => {
   return (
     <Bottom>
       <AlignLeft>
-        <p className="footer-text">Follow the MU:</p>
+        <p className="footer-text">Follow the MU on socials @WeAretheMU</p>
         <AlignRow>
-      <Link href="https://twitter.com/WeAreTheMU" target="_blank" title="Visit Musicians' Union on Twitter"><TwitterLogo/></Link>
+      <Link href="https://twitter.com/WeAreTheMU" target="_blank" title="Visit Musicians' Union on X (Twitter)"><XLogo/></Link>
+     <Link href="https://www.instagram.com/wearethemu" target="_blank" title="Visit Musicians' Union on Twitter"><InstagramLogo/></Link>
       <Link href="https://www.facebook.com/Musicians.Union" target="_blank" title="Visit Musicians' Union on Facebook"><FacebookLogo/></Link>
       </AlignRow>
       </AlignLeft>
       <AlignLeft>
-      <Link href='/about'>What is MU Safe Space</Link>
-      <Link href='/frequently-asked-questions'>What you need to know</Link>
+      <Link href='/about'>About MU Safe Space</Link>
+      <Link href='/frequently-asked-questions'>FAQs</Link>
       <Link href='/support'>Get help and support</Link>
       </AlignLeft>
     </Bottom>
